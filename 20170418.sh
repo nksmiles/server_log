48e40d84e190d7cc2cfb9031380f753aa69107bb
nksmiles <vico.song@outlook.com>
2017-04-18 14:17:16 +0800

    Create 20170418.sh

---
+++ 20170418.sh
@@ -0,0 +1,13 @@
+apt-get update
+
+apt-get install python-dev build-essential 
+
+apt-get install python-pip
+
+pip install --upgrade pip
+
+pip install --upgrade virtualenv
+
+apt-get install -y git
+
+pip install git+https://github.com/shadowsocks/shadowsocks.git@master
